1d20e201972502ed965a36cefa4ec6173ed2be44
AftabIB <aftab.bhadgaonkar@walchandsangli.ac.in>
2024-12-15 23:25:51 +0530

    Change in Logic

--- frontend/src/Components/ChatContainer.jsx
+++ frontend/src/Components/ChatContainer.jsx
@@ -3,10 +3,13 @@ import { useChatStore } from '../store/useChatStore'
 import ChatHeader from './ChatHeader.jsx';
 import MessageInput from './MessageInput.jsx';
 import MessageSkeleton from './skeletons/MessageSkeleton.jsx';
+import { useAuthStore } from "../store/useAuthStore";
+import {formatMessageTime} from "../lib/utils.js";
 
 const ChatContainer = () => {
   //fetch the messages
   const {messages, getMessages, isMessagesLoading, selectedUser} = useChatStore();
+  const {authUser} = useAuthStore();
   
   //we fetch the messages by, which current user is selected
   useEffect(() => {
@@ -28,11 +31,48 @@ const ChatContainer = () => {
     <div className="flex flex-1 flex-col overflow-auto">
       <ChatHeader />
 
-      <p>messages...</p>
+      <div className="flex-1 overflow-y-auto p-4 space-y-4">
+        {messages.map((message) => (
+          <div
+            key={message._id}
+            className={`chat ${
+              message.senderId === authUser._id ? "chat-end" : "chat-start"
+            }`}
+          >
+            <div className=" chat-image avatar">
+              <div className="size-10 rounded-full border">
+                <img
+                  src={
+                    message.senderId === authUser._id
+                      ? authUser.profilePic || "/avatar.png"
+                      : selectedUser.profilePic || "/avatar.png"
+                  }
+                  alt="profile pic"
+                />
+              </div>
+            </div>
+            <div className="chat-header mb-1">
+              <time className="text-xs opacity-50 ml-1">
+                {formatMessageTime(message.createdAt)}
+              </time>
+            </div>
+            <div className="chat-bubble flex flex-col">
+              {message.image && (
+                <img
+                  src={message.image}
+                  alt="Attachment"
+                  className="sm:max-w-[200px] rounded-md mt-1.5 mb-1.5"
+                />
+              )}
+              {message.text && <p>{message.text}</p>}
+            </div>
+          </div>
+        ))}
+      </div>
 
-      <MessageInput/>
+      <MessageInput />
     </div>
-  )
+  );
 }
 
 export default ChatContainer
